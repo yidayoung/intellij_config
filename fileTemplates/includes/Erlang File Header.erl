f10b22c8b348f6b1db1a6bd2d2071cdb25249d38
wangyida <wangyida@qiyi.com>
2020-03-31 10:21:00 +0800

    IC-2019.3.3 <wangyida@CDGAME-A1481-D Create markdown.xml, IntelliLang.xml, Lua.xml, project.default.xml, diff.xml, Default copy.xml, Default.xml, applicationLibraries.xml, ignore.xml, find.xml, SQL Script.sql, erlang.xml, keymap.xml, code.style.schemes.xml, Erlang File Header.erl, Darcula copy.icls, Default.xml, Lua File Header.lua, Erlang File Module.erl, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, Lua_File.Lua, Lua_config.Lua, user.xml, postfixTemplates.xml, Lua Skynet.Lua, Erlang.xml, ui.lnf.xml, filetypes.xml, projectView.xml, remote-servers.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, github.xml, templates.xml, emmy.xml, hg.xml, laf.xml, parameter.hints.xml, jdk.table.xml, path.macros.xml, Default _1_.xml, web-browsers.xml, wsl.distributions.xml, dbnavigator.xml, vcs.xml, usageView.xml, studioSettings.xml

---
+++ fileTemplates/includes/Erlang File Header.erl
@@ -0,0 +1,9 @@
+#parse("Erlang Custom Template Variables.erl")
+%%%-------------------------------------------------------------------
+%%% @author wangyida
+%%% @copyright (C) ${YEAR}, <SKYMOONS>
+%%% @doc
+%%%
+%%% @end
+%%% Created : ${DAY}. ${MONTH_NAME_SHORT} ${YEAR} ${TIME}
+%%%-------------------------------------------------------------------
---
+++ fileTemplates/includes/Lua File Header.lua
@@ -0,0 +1,9 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: wangyida
+-- Date: ${DATE}
+-- Time: ${TIME}
+-- To change this template use File | Settings | File Templates.
+--
+
+
